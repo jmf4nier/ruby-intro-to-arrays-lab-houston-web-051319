25c553144fafd9ff2b52022310d77481ce1c6cd8
Jason Fournier <jmf4nier@hotmail.com>
2019-04-30 23:03:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,17 +1,17 @@
-def my_new_array
+def instantiate_new_array
   []
 end   
-def my_two_array
+def array_with_two_elements
   [1,2]
 end 
-def my_first_element(array)
+def first_element(array)
   array[0]
 end
-def my_third_element(array)
+def third_element(array)
   array[2]
 end
 
-def my_last_element(array)
+def last_element(array)
   array[-1]
 end
 
