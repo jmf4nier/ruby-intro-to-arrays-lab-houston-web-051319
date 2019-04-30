659d65a8b3fb05fe9a5decaacd00e723521c9cd1
Jason Fournier <jmf4nier@hotmail.com>
2019-04-30 22:55:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,11 +1,17 @@
-my_new_array
+def my_new_array
+  []
+end   
+def my_two_array
+  [1,2]
+end 
+def my_first_element(array)
+  array[0]
+end
 
-my_two_array
-
-my_first_element
-
-my_last_element
+def my_last_element
 
 first_element_with_array_methods
 
 last_element_with_array_methods
+
+length_of_array
